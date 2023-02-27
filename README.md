55a3c0dd692a2b50324c75e98356ad5cee227a7b
Oscar Muñoz <oscar_m_f@hotmail.com>
2023-02-27 23:42:50 +0100

    OMF: First Release

---
+++ README.md
@@ -0,0 +1,28 @@
+# Cycling Climbing
+Suunto App Cycling Climbing
+
+This app is designed to show the meters climbed on each Ascent/Descent. It shows the time Speed on the Ascent, the meters Climbed and the number of Attempts/Run plus the Climb Angle Ascent. Saves in the SA the number of times we completed Ascent/Descent in a training. Saves in SA Suunto Plus the speed that you spend in Ascent and for each Ascent/Descent plus the Climb Angle Ascent. Generates a lap every time you finish the route or Ascent/Descent ***(you can also force that you have finished the Ascent/Descent or route by pressing the lap button).***
+
+### Screen Design:
+<img src="Small.png " width="150" height="150"> <img src="Medium.png " width="150" height="150"> <img src="Medium UI2.png " width="150" height="150"> <img src="Medium UI2 Plus.png " width="150" height="150"> <img src="Large.png " width="150" height="150">
+
+<br/>
+ 
+### SA Outputs:
+  #### Suunto Plus Metrics to analize later in SA
+  <img src="SuuntoPlusMetric.jpg" width="35%" height="35%">
+   <br/>
+   
+  #### SA Summary Outputs
+  <img src="SA_Metrics.jpg" width="35%" height="35%">
+   <br/>
+   
+## To be improve:
+  - More precision in climbAngleAscent, because it uses the distance without GSP.
+
+## I will Try to do it if possible:
+  - 15 different grading scales : https://www.guidedolomiti.com/en/rock-climbing-grades/
+
+---
+### :fire: My Stats :
+[![GitHub Streak](http://github-readme-streak-stats.herokuapp.com?user=osmufe&theme=submarine-flowers&hide_border=true&date_format=j%20M%5B%20Y%5D&mode=weekly&border=DD2727)](https://git.io/streak-stats)
---
+++ main.js
@@ -0,0 +1,128 @@
+/*
+ function onLap() { trace('--- onLap ---'); }
+ function onAutoLap() { trace('--- onAutoLap ---'); }
+ function onInterval() { trace('--- onInterval ---'); }
+ function onPoolLength() { trace('--- onPoolLength ---'); }
+ function onExerciseStart() { trace('--- onExerciseStart ---'); }
+ function onExercisePause() { trace('--- onExercisePause ---'); }
+ function onExerciseContinue() { trace('--- onExerciseContinue ---'); }
+ function onExerciseEnd() { trace('--- onExerciseEnd ---'); }
+ function onExercisePause() { trace('--- onExerciseEnd ---'); }
+ function onExerciseContinue() { trace('--- onExerciseEnd ---'); }
+ */
+
+// Output var climbAttempts: Number of times you make a route or ascent/descent in indoor climbing.
+// Output var climbDurationAscentDescent: The time it takes to complete a route or ascent/descent in indoor climbing.
+// Output var climbDurationAscent: The ascent in meters in indoor climbing.
+// main.js var climbDurationDescent: The time it takes to descend in indoor climbing.
+// main.js var climbTotalAscent: Total training climb minus current climb.
+// main.js var climbTotalDescent: Total training decrease minus current decrease.
+// Output var climbAttemptAscent: Ascent in meters of actual ascent or indoor climbing route.
+// main.js var climbAttemptDescent: Descent in meters of actual descent or indoor climbing route.
+// main.js var limbTotalDurationAscent: Total Time Duration Ascent in workout
+// main.js var climbTotalDurationDescent: Total Time Duration Descent in workout
+// main.js var Ascending: Value True o Fasle to detect if is Ascending
+// main.js var Descending: Value True o Fasle to detect if is Descending
+// Watch var DurationAscent: Total time in the workout that the watch calculate Ascent Time.
+// Watch var DurationDescent: Total time in the workout that the watch calculate Descent Time.
+// Watch var AscentMeters: Total Ascent meters in the workout.
+// Watch var DescentMeters: Total Descent meters in the workout.
+// .toFixed(0) Without decimals
+
+var Ascending, Descending, climbAttemptDescent, climbTotalAscent, climbTotalDescent, climbDistanceAttempAscent,
+    climbDistanceStartAttempAscent, climbRightTriangle;
+
+function evaluate(input, output) {  
+  output.climbAttemptAscent = input.AscentMeters.toFixed(0) - climbTotalAscent;
+  climbAttemptDescent = input.DescentMeters.toFixed(0) - climbTotalDescent;
+  if ((output.climbAttemptAscent > 0) && (climbAttemptDescent == 0)) {
+    if (climbDistanceStartAttempAscent == 0) {
+     // Save the distance when start the Ascent
+     climbDistanceStartAttempAscent = input.Distance
+    }else{
+     // Save the Speed
+     output.climbCadenceAscent = input.Cadence
+     // Save the Distance in meters when ascensing because later generate the angle of each Attempt
+     climbDistanceAttempAscent = input.Distance - climbDistanceStartAttempAscent;
+     if (climbDistanceAttempAscent > output.climbAttemptAscent) {
+      // Pythagoras theorem ( Calc the Right Triangle )
+      climbRightTriangle = Math.sqrt(Math.pow(climbDistanceAttempAscent, 2) - Math.pow(output.climbAttemptAscent, 2));
+      // Calculate the angle of Cos
+      output.climbAngleAscent = Math.acos(climbRightTriangle/climbDistanceAttempAscent)*(180/Math.PI);;
+     } else if (climbDistanceAttempAscent == output.climbAttemptAscent){
+      output.climbAngleAscent = 90;
+     } else {
+      output.climbAngleAscent = 0;
+     }
+    }
+    Ascending = true; 
+    Ascending = false;   
+  } else if ((climbAttemptDescent > 0) && (output.climbAttemptAscent > 0)) {
+    Descending = true;
+    Ascending = false;
+  }else {
+    climbTotalDescent = input.DescentMeters.toFixed(0);  
+  }
+  if ((output.climbAttemptAscent <= climbAttemptDescent) && 
+  ((Ascending == false ) && (Descending == true))) {
+   // Trigger lap once
+   $.put("/Activity/Trigger", 0);
+ }  
+}
+ 
+function onExerciseStart(input, output) {
+  // Initializing Variables 
+  output.climbCadenceAscent = 0;
+  output.climbAttempts = 0;
+  climbTotalAscent = 0;
+  climbTotalDescent = 0;
+  output.climbAttemptAscent = 0;
+  climbAttemptDescent = 0;
+  climbDistanceAttempAscent = 0;
+  climbDistanceStartAttempAscent = 0;
+  climbRightTriangle = 0;
+  Ascending = 'false';
+  Descending = 'false';
+}
+
+function onLap(input, output) { 
+  // Collect the latest data
+  // Use this var to save the data on SA for each lap
+  climbDistanceAttempAscent = input.Distance - climbDistanceStartAttempAscent;
+
+  // Initializing Variables for new Ascent and increase output.climbAttempts Variable
+  output.climbCadenceAscent = 0;
+  output.climbAttemptAscent = 0;
+  climbAttemptDescent = 0;
+  climbDistanceAttempAscent = 0;
+  climbDistanceStartAttempAscent = 0;
+  output.climbAngleAscent = 0;
+  climbRightTriangle = 0;
+  climbTotalAscent = input.AscentMeters.toFixed(0);
+  climbTotalDescent = input.DescentMeters.toFixed(0);
+  output.climbAttempts = output.climbAttempts + 1;
+  Ascending = 'false';
+  Descending = 'false';
+}
+ 
+ function getUserInterface(input, output) {
+   return {
+    template: 't'
+   };
+ }
+ 
+ // This is called also when user backs from exercise start panel without starting
+ // exercise. onExerciseEnd() is not working at all as zapp gets disabled before
+ // it is called (and it would be called only when exercise is really started).
+ function getSummaryOutputs(input, output) {
+   return [
+     {
+      // Save the data of number of times you make a route or ascent/descent in indoor climbing into SA.
+      id: 'climbAttempts',
+      name: "Number of Ascent",
+      format: 'Count_Threedigits',
+      value: output.climbAttempts
+     },
+
+   ];
+ }
